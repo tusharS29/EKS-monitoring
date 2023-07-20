54330f3f4770f5e33126d8ba1828ad52cde5e7ee
tushu-S <tushar@cateina.com>
2023-07-20 16:08:01 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# EKS-monitoring:
+
+<h1> Resources Used: </h1>
+
+EKS 
+ECR
+Flask
+Docker
---
+++ ecr.py
@@ -0,0 +1,9 @@
+import boto3
+
+ecr_client = boto3.client('ecr',region_name = 'us-east-1')
+
+repository_name = "flask"
+response = ecr_client.create_repository(repositoryName=repository_name)
+
+repository_uri = response ['repository']['repositoryUri']
+print(repository_uri)
